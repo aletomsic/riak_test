6b0211aeb6433e3a4bc0554ae200a6bfc0a0a009
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2014-05-22 14:53:32 +0200

    First test of floppystore.

--- tests/floppystore.erl
+++ tests/floppystore.erl
@@ -7,6 +7,18 @@
 -define(HARNESS, (rt_config:get(rt_harness))).
 
 confirm() ->
-    Nodes = rt:build_clusters([3]),
+    simple_test().
+
+simple_test() ->
+    [Nodes] = rt:build_clusters([1]),
+
+    FirstNode = hd(Nodes),
     lager:info("Nodes: ~p", [Nodes]),
+
+    WriteResult = rpc:call(FirstNode, floppy, append, [abc, {increment, 4}]),
+    ?assertMatch({ok, _}, WriteResult),
+
+    ReadResult = rpc:call(FirstNode, floppy, read, [abc, riak_dt_gcounter]),
+    ?assertEqual(1, ReadResult),
+
     ok.
